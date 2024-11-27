8affbbe39864523e1f827d6f54fd9b02fd8b867a
Antonel Gabriel Crișan <antonel19.gabriel@gmail.com>
2024-11-27 19:32:30 +0200

    Modificare executabil

--- src/main/java/org/energy_management/energymanagement/Executabil.java
+++ src/main/java/org/energy_management/energymanagement/Executabil.java
@@ -2,6 +2,7 @@ package org.energy_management.energymanagement;
 
 public class Executabil {
     public static void main(String[] args) {
+        ModbusReader.main(args);
         AppRun.main(args);
     }
 }
